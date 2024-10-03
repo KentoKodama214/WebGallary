50a1aa9c0bf337a09888c43b6a8194d7d6c829d8
KentoKodama214 <kodaken214@gmail.com>
2024-10-04 06:52:22 +0900

    PhotoDetailMapperクラスのユニテを実装

--- src/test/java/com/web/gallary/mapper/PhotoDetailMapperTest.java
+++ src/test/java/com/web/gallary/mapper/PhotoDetailMapperTest.java
@@ -2,6 +2,13 @@ package com.web.gallary.mapper;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Nested;
@@ -13,6 +20,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+
+import com.web.gallary.dto.PhotoDetailDto;
+import com.web.gallary.dto.PhotoDetailGetDto;
+import com.web.gallary.dto.PhotoDto;
+import com.web.gallary.dto.PhotoListGetDto;
 
 @MybatisTest
 @ActiveProfiles("test")
@@ -30,13 +42,42 @@ public class PhotoDetailMapperTest {
 		@Order(1)
 		@DisplayName("正常系：selectで1件以上の場合")
 		void getPhotoList_some_photos() {
-			assertTrue(false);
+			PhotoListGetDto photoSelectDto = new PhotoListGetDto();
+			photoSelectDto.setAccountNo(1);
+			photoSelectDto.setPhotoAccountNo(1);
+			
+			List<PhotoDto> actual = photoDetailMapper.getPhotoList(photoSelectDto);
+			
+			PhotoDto actualPhotoDto1 = actual.stream().sorted(Comparator.comparing(PhotoDto::getPhotoNo)).toList().getFirst();
+			assertEquals(1, actualPhotoDto1.getAccountNo());
+			assertEquals(1, actualPhotoDto1.getPhotoNo());
+			assertEquals(2, actualPhotoDto1.getFavoriteCount());
+			assertEquals(true, actualPhotoDto1.getIsFavorite());
+			assertEquals(OffsetDateTime.of(2021, 1, 1, 0, 0, 0, 0, ZoneOffset.ofHours(0)), actualPhotoDto1.getPhotoAt());
+			assertEquals("https://www.xxx.com/DSC111.jpg", actualPhotoDto1.getImageFilePath());
+			assertEquals("キャプション11", actualPhotoDto1.getCaption());
+			assertEquals("vertical", actualPhotoDto1.getDirectionKbnCode());
+			
+			PhotoDto actualPhotoDto2 = actual.stream().sorted(Comparator.comparing(PhotoDto::getPhotoNo)).toList().getLast();
+			assertEquals(1, actualPhotoDto2.getAccountNo());
+			assertEquals(2, actualPhotoDto2.getPhotoNo());
+			assertEquals(1, actualPhotoDto2.getFavoriteCount());
+			assertEquals(false, actualPhotoDto2.getIsFavorite());
+			assertEquals(OffsetDateTime.of(2021, 2, 1, 0, 0, 0, 0, ZoneOffset.ofHours(0)), actualPhotoDto2.getPhotoAt());
+			assertEquals("https://www.xxx.com/DSC222.jpg", actualPhotoDto2.getImageFilePath());
+			assertEquals("キャプション12", actualPhotoDto2.getCaption());
+			assertEquals("horizontal", actualPhotoDto2.getDirectionKbnCode());
 		}
 		@Test
 		@Order(2)
 		@DisplayName("正常系：selectで0件の場合")
 		void getPhotoList_not_found() {
-			assertTrue(false);
+			PhotoListGetDto photoSelectDto = new PhotoListGetDto();
+			photoSelectDto.setAccountNo(1);
+			photoSelectDto.setPhotoAccountNo(3);
+			
+			List<PhotoDto> actual = photoDetailMapper.getPhotoList(photoSelectDto);
+			assertEquals(new ArrayList<PhotoDto>(), actual);
 		}
 	}
 	
@@ -49,13 +90,42 @@ public class PhotoDetailMapperTest {
 		@Order(1)
 		@DisplayName("正常系：selectで1件の場合")
 		void getPhotoDetail_found() {
-			assertTrue(false);
+			PhotoDetailGetDto photoGetDto = new PhotoDetailGetDto();
+			photoGetDto.setAccountNo(1);
+			photoGetDto.setPhotoAccountNo(1);
+			photoGetDto.setPhotoNo(1);
+			
+			PhotoDetailDto actual = photoDetailMapper.getPhotoDetail(photoGetDto);
+			assertEquals(1, actual.getAccountNo());
+			assertEquals(1, actual.getPhotoNo());
+			assertEquals(true, actual.getIsFavorite());
+			assertEquals(OffsetDateTime.of(2021, 1, 1, 0, 0, 0, 0, ZoneOffset.ofHours(0)), actual.getPhotoAt());
+			assertEquals(1, actual.getLocationNo());
+			assertEquals("住所1", actual.getAddress());
+			assertTrue(BigDecimal.valueOf(38.1).compareTo(actual.getLatitude()) == 0);
+			assertTrue(BigDecimal.valueOf(115.1).compareTo(actual.getLongitude()) == 0);
+			assertEquals("ロケーション1", actual.getLocationName());
+			assertEquals("https://www.xxx.com/DSC111.jpg", actual.getImageFilePath());
+			assertEquals("タイトル11", actual.getPhotoJapaneseTitle());
+			assertEquals("title11", actual.getPhotoEnglishTitle());
+			assertEquals("キャプション11", actual.getCaption());
+			assertEquals("vertical", actual.getDirectionKbnCode());
+			assertEquals(24, actual.getFocalLength());
+			assertTrue(BigDecimal.valueOf(8.0).compareTo(actual.getFValue()) == 0);
+			assertTrue(BigDecimal.valueOf(1).compareTo(actual.getShutterSpeed()) == 0);
+			assertEquals(100, actual.getIso());
 		}
 		@Test
 		@Order(2)
 		@DisplayName("正常系：selectで0件の場合")
 		void getPhotoDetail_not_found() {
-			assertTrue(false);
+			PhotoDetailGetDto photoGetDto = new PhotoDetailGetDto();
+			photoGetDto.setAccountNo(1);
+			photoGetDto.setPhotoAccountNo(3);
+			photoGetDto.setPhotoNo(1);
+			
+			PhotoDetailDto actual = photoDetailMapper.getPhotoDetail(photoGetDto);
+			assertNull(actual);
 		}
 	}
 }
--- src/test/resources/sql/mapper/PhotoDetailMapperTest.sql
+++ src/test/resources/sql/mapper/PhotoDetailMapperTest.sql
@@ -1,10 +1,28 @@
 -- common.account
+insert into common.account values(1,  1,  '2000-01-01 09:00:00 Asia/Tokyo', 1,  '2001-01-01 09:00:00 Asia/Tokyo', false, 'aaaaaaaa', 'AAAAAAAA', '$2a$10$password1',  '1991-02-14', '',      '',         '',        '',         'administrator', '2002-01-01 09:00:00 Asia/Tokyo', 0);
+insert into common.account values(2,  2,  '2000-01-02 09:00:00 Asia/Tokyo', 2,  '2001-01-02 09:00:00 Asia/Tokyo', false, 'bbbbbbbb', 'BBBBBBBB', '$2a$10$password2',  '1900-01-01', 'man',   '',         '',        '',         'administrator', '2002-01-01 09:00:00 Asia/Tokyo', 0);
+insert into common.account values(3,  3,  '2000-01-03 09:00:00 Asia/Tokyo', 3,  '2001-01-03 09:00:00 Asia/Tokyo', false, 'cccccccc', 'CCCCCCCC', '$2a$10$password3',  '1900-01-01', '',      'Hokkaido', '',        '',         'administrator', '2002-01-01 09:00:00 Asia/Tokyo', 0);
+insert into common.account values(4,  4,  '2000-01-04 09:00:00 Asia/Tokyo', 4,  '2001-01-04 09:00:00 Asia/Tokyo', false, 'dddddddd', 'DDDDDDDD', '$2a$10$password4',  '1900-01-01', '',      '',         'Okinawa', '',         'administrator', '2002-01-01 09:00:00 Asia/Tokyo', 0);
+insert into common.account values(5,  5,  '2000-01-05 09:00:00 Asia/Tokyo', 5,  '2001-01-05 09:00:00 Asia/Tokyo', false, 'eeeeeeee', 'EEEEEEEE', '$2a$10$password5',  '1900-01-01', '',      '',         '',        'フリーメモ', 'administrator', '2002-01-01 09:00:00 Asia/Tokyo', 0);
 
 
 -- common.location_mst
-
+insert into common.location_mst values(1, 1, 1, '2000-01-05 09:00:00 Asia/Tokyo', 1, '2000-01-05 09:00:00 Asia/Tokyo', false, 'ロケーション1', '住所1', 38.100, 115.100);
+insert into common.location_mst values(1, 2, 1, '2000-01-05 09:00:00 Asia/Tokyo', 1, '2000-01-05 09:00:00 Asia/Tokyo', false, 'ロケーション2', '住所2', 38.200, 115.200);
+insert into common.location_mst values(1, 3, 1, '2000-01-05 09:00:00 Asia/Tokyo', 1, '2000-01-05 09:00:00 Asia/Tokyo', true, 'ロケーション3', '住所3', 38.300, 115.300);
+insert into common.location_mst values(2, 1, 1, '2000-01-05 09:00:00 Asia/Tokyo', 1, '2000-01-05 09:00:00 Asia/Tokyo', false, 'ロケーション4', '住所4', 38.400, 115.400);
+insert into common.location_mst values(2, 2, 1, '2000-01-05 09:00:00 Asia/Tokyo', 1, '2000-01-05 09:00:00 Asia/Tokyo', true, 'ロケーション5', '住所5', 38.500, 115.500);
 
 -- photo.photo_mst
-
+insert into photo.photo_mst values(1, 1, 1, '2000-01-01 09:00:00 Asia/Tokyo', 1, '2000-01-01 09:00:00 Asia/Tokyo', false, '2021-01-01 09:00:00 Asia/Tokyo', 1, 'https://www.xxx.com/DSC111.jpg', 'タイトル11', 'title11', 'キャプション11', 'vertical',   24, 8.0,  1,  100);
+insert into photo.photo_mst values(1, 2, 1, '2000-01-01 09:00:00 Asia/Tokyo', 1, '2000-01-01 09:00:00 Asia/Tokyo', false, '2021-02-01 09:00:00 Asia/Tokyo', 2, 'https://www.xxx.com/DSC222.jpg', 'タイトル12', 'title12', 'キャプション12', 'horizontal', 36, 9.0,  2,  200);
+insert into photo.photo_mst values(1, 3, 1, '2000-01-01 09:00:00 Asia/Tokyo', 1, '2000-01-01 09:00:00 Asia/Tokyo', true,  '2021-03-01 09:00:00 Asia/Tokyo', 3, 'https://www.xxx.com/DSC333.jpg', 'タイトル13', 'title13', 'キャプション13', 'horizontal', 50, 10.0, 3,  400);
+insert into photo.photo_mst values(2, 1, 1, '2000-01-01 09:00:00 Asia/Tokyo', 1, '2000-01-01 09:00:00 Asia/Tokyo', false, '2022-01-01 09:00:00 Asia/Tokyo', 4, 'https://www.xxx.com/DSC444.jpg', 'タイトル21', 'title21', 'キャプション21', 'horizontal', 80, 12.0, 5,  800);
+insert into photo.photo_mst values(2, 2, 1, '2000-01-01 09:00:00 Asia/Tokyo', 1, '2000-01-01 09:00:00 Asia/Tokyo', false, '2022-02-01 09:00:00 Asia/Tokyo', 5, 'https://www.xxx.com/DSC555.jpg', 'タイトル22', 'title22', 'キャプション22', 'horizontal', 90, 16.0, 10, 1200);
 
 -- photo.photo_favorite
+insert into photo.photo_favorite values(1, 1, 1, 1, now());
+--insert into photo.photo_favorite values(1, 1, 2, 1, now());
+insert into photo.photo_favorite values(2, 1, 1, 1, now());
+insert into photo.photo_favorite values(2, 1, 2, 1, now());
+insert into photo.photo_favorite values(2, 2, 1, 1, now());
