508ec9337179a08f77c0cc76b81d89174b91ca5b
KentoKodama214 <kodaken214@gmail.com>
2024-10-08 06:39:18 +0900

    ComminControllerAdviceクラスのユニテを実装

--- src/test/java/com/web/gallary/controller/CommonControllerAdviceTest.java
+++ src/test/java/com/web/gallary/controller/CommonControllerAdviceTest.java
@@ -1,6 +1,11 @@
 package com.web.gallary.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
@@ -8,13 +13,27 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.servlet.ModelAndView;
 
-@SpringBootTest
+import com.web.gallary.enumuration.ErrorValues;
+import com.web.gallary.exception.ForbiddenAccountException;
+import com.web.gallary.exception.PhotoNotFoundException;
+import com.web.gallary.helper.SessionHelper;
+
 @ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
 public class CommonControllerAdviceTest {
+	@InjectMocks
+	private CommonControllerAdvice commonControllerAdvice;
 	
+	@Mock
+	private SessionHelper sessionHelper;
 	
 	@Nested
 	@Order(1)
@@ -24,14 +43,33 @@ public class CommonControllerAdviceTest {
 		@Order(1)
 		@DisplayName("正常系：非ログインユーザーの場合")
 		void handleFileForbiddenAccountException_not_login_user() {
-			assertTrue(false);
+			doReturn(null).when(sessionHelper).getAccountId();
+			ForbiddenAccountException exception = new ForbiddenAccountException(ErrorValues.EC0000);
+			
+			ModelAndView actual = commonControllerAdvice.handleFileForbiddenAccountException(exception);
+			assertEquals("error_page", actual.getViewName());
+			Map<String, Object> models = actual.getModel();
+			assertEquals(HttpStatus.FORBIDDEN.value(), (int)models.get("httpStatus"));
+			assertEquals(ErrorValues.EC0000.getErrorCode(), models.get("errorCode").toString());
+			assertEquals(ErrorValues.EC0000.getErrorMessage(), models.get("errorMessage").toString());
+			assertEquals("/login", models.get("goBackPageUrl").toString());
 		}
 		
 		@Test
 		@Order(2)
 		@DisplayName("正常系：ログインユーザーの場合")
 		void handleFileForbiddenAccountException_login_user() {
-			assertTrue(false);
+			String accountId = "aaaaaaaa";
+			doReturn(accountId).when(sessionHelper).getAccountId();
+			ForbiddenAccountException exception = new ForbiddenAccountException(ErrorValues.EC0000);
+			
+			ModelAndView actual = commonControllerAdvice.handleFileForbiddenAccountException(exception);
+			assertEquals("error_page", actual.getViewName());
+			Map<String, Object> models = actual.getModel();
+			assertEquals(HttpStatus.FORBIDDEN.value(), (int)models.get("httpStatus"));
+			assertEquals(ErrorValues.EC0000.getErrorCode(), models.get("errorCode").toString());
+			assertEquals(ErrorValues.EC0000.getErrorMessage(), models.get("errorMessage").toString());
+			assertEquals("/photo/" + accountId + "/photo_list", models.get("goBackPageUrl").toString());
 		}
 	}
 	
@@ -43,16 +81,36 @@ public class CommonControllerAdviceTest {
 		@Order(1)
 		@DisplayName("正常系：非ログインユーザーの場合")
 		void handlePhotoNotFoundException_not_login_user() {
-			assertTrue(false);
+			doReturn(null).when(sessionHelper).getAccountId();
+			PhotoNotFoundException exception = new PhotoNotFoundException(ErrorValues.EP0001);
+			
+			ModelAndView actual = commonControllerAdvice.handlePhotoNotFoundException(exception);
+			assertEquals("error_page", actual.getViewName());
+			Map<String, Object> models = actual.getModel();
+			assertEquals(HttpStatus.BAD_REQUEST.value(), (int)models.get("httpStatus"));
+			assertEquals(ErrorValues.EP0001.getErrorCode(), models.get("errorCode").toString());
+			assertEquals(ErrorValues.EP0001.getErrorMessage(), models.get("errorMessage").toString());
+			assertEquals("/login", models.get("goBackPageUrl").toString());
 		}
 		
 		@Test
 		@Order(2)
 		@DisplayName("正常系：ログインユーザーの場合")
 		void handlePhotoNotFoundException_login_user() {
-			assertTrue(false);
+			String accountId = "aaaaaaaa";
+			doReturn(accountId).when(sessionHelper).getAccountId();
+			PhotoNotFoundException exception = new PhotoNotFoundException(ErrorValues.EP0001);
+			
+			ModelAndView actual = commonControllerAdvice.handlePhotoNotFoundException(exception);
+			assertEquals("error_page", actual.getViewName());
+			Map<String, Object> models = actual.getModel();
+			assertEquals(HttpStatus.BAD_REQUEST.value(), (int)models.get("httpStatus"));
+			assertEquals(ErrorValues.EP0001.getErrorCode(), models.get("errorCode").toString());
+			assertEquals(ErrorValues.EP0001.getErrorMessage(), models.get("errorMessage").toString());
+			assertEquals("/photo/" + accountId + "/photo_list", models.get("goBackPageUrl").toString());
 		}
 	}
+	
 	
 	@Nested
 	@Order(3)
@@ -61,15 +119,21 @@ public class CommonControllerAdviceTest {
 		@Test
 		@Order(1)
 		@DisplayName("正常系：非ログインユーザーの場合")
-		void getGoBackPageUrl_not_login_user() {
-			assertTrue(false);
+		void getGoBackPageUrl_not_login_user() throws NoSuchMethodException, SecurityException, IllegalAccessException, InvocationTargetException {
+			Method goBackPageUrl = CommonControllerAdvice.class.getDeclaredMethod("getGoBackPageUrl");
+			goBackPageUrl.setAccessible(true);
+			doReturn(null).when(sessionHelper).getAccountId();
+			assertEquals("/login", (String) goBackPageUrl.invoke(commonControllerAdvice));
 		}
 		
 		@Test
 		@Order(2)
 		@DisplayName("正常系：ログインユーザーの場合")
-		void getGoBackPageUrl_login_user() {
-			assertTrue(false);
+		void getGoBackPageUrl_login_user() throws NoSuchMethodException, SecurityException, IllegalAccessException, InvocationTargetException {
+			Method goBackPageUrl = CommonControllerAdvice.class.getDeclaredMethod("getGoBackPageUrl");
+			goBackPageUrl.setAccessible(true);
+			doReturn("aaaaaaaa").when(sessionHelper).getAccountId();
+			assertEquals("/photo/aaaaaaaa/photo_list", (String) goBackPageUrl.invoke(commonControllerAdvice));
 		}
 	}
 }
