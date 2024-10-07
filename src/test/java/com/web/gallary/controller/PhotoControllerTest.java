ca169c01cd32d9eaf268b7eabfc7defdc3ca97f2
KentoKodama214 <kodaken214@gmail.com>
2024-10-07 20:11:31 +0900

    PhotoControllerクラスのユニテを実装

--- src/test/java/com/web/gallary/controller/PhotoControllerTest.java
+++ src/test/java/com/web/gallary/controller/PhotoControllerTest.java
@@ -1,6 +1,9 @@
 package com.web.gallary.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Map;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
@@ -8,13 +11,36 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.servlet.ModelAndView;
 
-@SpringBootTest
+import com.web.gallary.config.PhotoConfig;
+import com.web.gallary.entity.Account;
+import com.web.gallary.helper.SessionHelper;
+import com.web.gallary.service.impl.AccountServiceImpl;
+import com.web.gallary.service.impl.PhotoServiceImpl;
+
 @ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
 public class PhotoControllerTest {
-
+	@InjectMocks
+	private PhotoController photoController;
+	
+	@Mock
+	private PhotoServiceImpl photoServiceImpl;
+	
+	@Mock
+	private AccountServiceImpl accountServiceImpl;
+	
+	@Mock
+	private SessionHelper sessionHelper;
+	
+	@Mock
+	private PhotoConfig photoConfig;
 	
 	@Nested
 	@Order(1)
@@ -24,14 +50,56 @@ public class PhotoControllerTest {
 		@Order(1)
 		@DisplayName("正常系：非ログインユーザーの場合")
 		void photoList_not_login_user() {
-			assertTrue(false);
+			String photoAccountId = "aaaaaaaa";
+			String accountName = "AAAAAAAA";
+			doReturn(null).when(sessionHelper).getAccountNo();
+			doReturn(true).when(photoServiceImpl).isReachedUpperLimit(null);
+			doReturn(null).when(sessionHelper).getAccountId();
+			Account account = Account.builder().accountId(photoAccountId).accountName(accountName).build();
+			doReturn(account).when(accountServiceImpl).getAccountById(photoAccountId);
+			
+			ModelAndView  actual = photoController.photoList(photoAccountId);
+			Map<String, Object> models = actual.getModel();
+			assertEquals("photo_list", actual.getViewName());
+			assertEquals(7, models.size());
+			assertEquals(photoAccountId, models.get("ownerId").toString());
+			assertEquals(false, (Boolean) models.get("isOwner"));
+			assertEquals(false, (Boolean) models.get("isAbleToAddPhoto"));
+			assertEquals(accountName, models.get("accountName").toString());
+			assertEquals("/account_list", models.get("account_list_url").toString());
+			assertEquals("/photo/" + photoAccountId + "/photo_list", models.get("photo_list_url").toString());
+			assertEquals("/photo/" + photoAccountId + "/photo_detail", models.get("photo_detail_url").toString());
 		}
 		
 		@Test
 		@Order(2)
 		@DisplayName("正常系：ログインユーザー（ページ所有者でない）の場合")
 		void photoList_login_user_not_owner() {
-			assertTrue(false);
+			String photoAccountId = "aaaaaaaa";
+			String accountName = "AAAAAAAA";
+			String loginAccountId = "bbbbbbbb";
+			doReturn(1).when(sessionHelper).getAccountNo();
+			doReturn(true).when(photoServiceImpl).isReachedUpperLimit(1);
+			doReturn(loginAccountId).when(sessionHelper).getAccountId();
+			Account account = Account.builder().accountId(photoAccountId).accountName(accountName).build();
+			doReturn(account).when(accountServiceImpl).getAccountById(photoAccountId);
+			
+			ModelAndView  actual = photoController.photoList(photoAccountId);
+			Map<String, Object> models = actual.getModel();
+			assertEquals("photo_list", actual.getViewName());
+			assertEquals(12, models.size());
+			assertEquals(photoAccountId, models.get("ownerId").toString());
+			assertEquals(false, (Boolean) models.get("isOwner"));
+			assertEquals(false, (Boolean) models.get("isAbleToAddPhoto"));
+			assertEquals(accountName, models.get("accountName").toString());
+			assertEquals("/account_list", models.get("account_list_url").toString());
+			assertEquals("/photo/" + photoAccountId + "/photo_list", models.get("photo_list_url").toString());
+			assertEquals("/photo/" + photoAccountId + "/photo_detail", models.get("photo_detail_url").toString());
+			assertEquals("/" + loginAccountId + "/account_setting", models.get("account_setting_url").toString());
+			assertEquals("/photo/" + loginAccountId + "/photo_list", models.get("my_photo_list_url").toString());
+			assertEquals("/photo/" + loginAccountId + "/photo_setting", models.get("photo_setting_url").toString());
+			assertEquals("/photo/" + loginAccountId + "/delete", models.get("photo_delete_url").toString());
+			assertEquals("/photo/" + loginAccountId + "/save", models.get("photo_save_url").toString());
 		}
 		
 		@Test
