961c26349439bca5d10271c3f9e4022d52532406
KentoKodama214 <kodaken214@gmail.com>
2024-10-16 20:28:49 +0900

    KbnMstServiceImplとPhotoFavoriteServiceImplの結合テストを実装

--- src/test/java/com/web/gallary/service/impl/integration/PhotoFavoriteServiceImplIntegrationTest.java
+++ src/test/java/com/web/gallary/service/impl/integration/PhotoFavoriteServiceImplIntegrationTest.java
@@ -2,6 +2,9 @@ package com.web.gallary.service.impl.integration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.OffsetDateTime;
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Nested;
@@ -16,8 +19,10 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.web.gallary.entity.PhotoFavorite;
 import com.web.gallary.exception.RegistFailureException;
 import com.web.gallary.exception.UpdateFailureException;
+import com.web.gallary.model.PhotoFavoriteModel;
 import com.web.gallary.service.impl.PhotoFavoriteServiceImpl;
 
 @ActiveProfiles("test")
@@ -40,14 +45,41 @@ public class PhotoFavoriteServiceImplIntegrationTest {
 		@Order(1)
 		@DisplayName("正常系")
 		void addFavorite_success() throws RegistFailureException {
-			assertTrue(false);
+			PhotoFavoriteModel photoFavoriteModel = PhotoFavoriteModel.builder()
+					.accountNo(2)
+					.favoritePhotoAccountNo(1)
+					.favoritePhotoNo(1)
+					.build();
+			
+			photoFavoriteServiceImpl.addFavorite(photoFavoriteModel);
+			
+			List<PhotoFavorite> actualData = jdbcTemplate.query(
+					"SELECT * FROM photo.photo_favorite WHERE account_no=2 and favorite_photo_account_no=1 and favorite_photo_no=1", (rs, rowNum) ->
+						PhotoFavorite.builder()
+							.accountNo(rs.getInt("account_no"))
+							.favoritePhotoAccountNo(rs.getInt("favorite_photo_account_no"))
+							.favoritePhotoNo(rs.getInt("favorite_photo_no"))
+							.createdBy(rs.getInt("created_by"))
+							.createdAt(rs.getObject("created_at", OffsetDateTime.class))
+							.build());
+			assertEquals(1, actualData.size());
+			assertEquals(2, actualData.getFirst().getAccountNo());
+			assertEquals(1, actualData.getFirst().getFavoritePhotoAccountNo());
+			assertEquals(1, actualData.getFirst().getFavoritePhotoNo());
+			assertEquals(2, actualData.getFirst().getCreatedBy());
 		}
 		
 		@Test
 		@Order(2)
 		@DisplayName("異常系：RegistFailureExceptionをthrowする")
 		void addFavorite_RegistFailureException() throws RegistFailureException {
-			assertTrue(false);
+			PhotoFavoriteModel photoFavoriteModel = PhotoFavoriteModel.builder()
+					.accountNo(1)
+					.favoritePhotoAccountNo(1)
+					.favoritePhotoNo(1)
+					.build();
+			
+			assertThrows(RegistFailureException.class, () -> photoFavoriteServiceImpl.addFavorite(photoFavoriteModel));
 		}
 	}
 	
@@ -60,14 +92,48 @@ public class PhotoFavoriteServiceImplIntegrationTest {
 		@Order(1)
 		@DisplayName("正常系")
 		void deleteFavorite_success() throws UpdateFailureException {
-			assertTrue(false);
+			PhotoFavoriteModel photoFavoriteModel = PhotoFavoriteModel.builder()
+					.accountNo(1)
+					.favoritePhotoAccountNo(1)
+					.favoritePhotoNo(1)
+					.build();
+			
+			photoFavoriteServiceImpl.deleteFavorite(photoFavoriteModel);
+			
+			List<PhotoFavorite> actualData = jdbcTemplate.query(
+			"SELECT * FROM photo.photo_favorite WHERE account_no=1 and favorite_photo_account_no=2 and favorite_photo_no=1", (rs, rowNum) ->
+				PhotoFavorite.builder()
+					.accountNo(rs.getInt("account_no"))
+					.favoritePhotoAccountNo(rs.getInt("favorite_photo_account_no"))
+					.favoritePhotoNo(rs.getInt("favorite_photo_no"))
+					.createdBy(rs.getInt("created_by"))
+					.createdAt(rs.getObject("created_at", OffsetDateTime.class))
+					.build());
+			assertEquals(0, actualData.size());
+			
+			List<PhotoFavorite> actualRestData = jdbcTemplate.query(
+					"SELECT * FROM photo.photo_favorite", (rs, rowNum) ->
+						PhotoFavorite.builder()
+							.accountNo(rs.getInt("account_no"))
+							.favoritePhotoAccountNo(rs.getInt("favorite_photo_account_no"))
+							.favoritePhotoNo(rs.getInt("favorite_photo_no"))
+							.createdBy(rs.getInt("created_by"))
+							.createdAt(rs.getObject("created_at", OffsetDateTime.class))
+							.build());
+			assertEquals(3, actualRestData.size());
 		}
 		
 		@Test
 		@Order(2)
 		@DisplayName("異常系：UpdateFailureExceptionをthrowする")
 		void deleteFavorite_UpdateFailureException() throws UpdateFailureException {
-			assertTrue(false);
+			PhotoFavoriteModel photoFavoriteModel = PhotoFavoriteModel.builder()
+					.accountNo(9)
+					.favoritePhotoAccountNo(1)
+					.favoritePhotoNo(1)
+					.build();
+			
+			assertThrows(UpdateFailureException.class, () ->photoFavoriteServiceImpl.deleteFavorite(photoFavoriteModel));
 		}
 	}
 }
