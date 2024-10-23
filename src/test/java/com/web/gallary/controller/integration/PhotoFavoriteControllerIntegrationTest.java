f4b5ee3b5656cb35726e174ca96dcc642a1f1ae5
KentoKodama214 <kodaken214@gmail.com>
2024-10-24 06:43:43 +0900

    PhotoFavoriteControllerの結合テストを実装。一部プロダクトコードの不備を修正

--- src/test/java/com/web/gallary/controller/integration/PhotoFavoriteControllerIntegrationTest.java
+++ src/test/java/com/web/gallary/controller/integration/PhotoFavoriteControllerIntegrationTest.java
@@ -1,6 +1,12 @@
 package com.web.gallary.controller.integration;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.time.OffsetDateTime;
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
@@ -9,25 +15,35 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.web.gallary.controller.PhotoFavoriteController;
-import com.web.gallary.exception.BadRequestException;
-import com.web.gallary.exception.RegistFailureException;
-import com.web.gallary.exception.UpdateFailureException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.web.gallary.AccountPrincipal;
+import com.web.gallary.controller.request.PhotoFavoriteDeleteRequest;
+import com.web.gallary.controller.request.PhotoFavoriteRegistRequest;
+import com.web.gallary.entity.Account;
+import com.web.gallary.entity.PhotoFavorite;
+import com.web.gallary.enumuration.ErrorValues;
 
 @ActiveProfiles("test")
-@SpringBootTest(webEnvironment = WebEnvironment.NONE)
+@SpringBootTest
 @Transactional
+@AutoConfigureMockMvc
 @Sql("/sql/common/ResetAccountNoSeq.sql")
 public class PhotoFavoriteControllerIntegrationTest {
 	@Autowired
-	private PhotoFavoriteController photoFavoriteController;
+    private MockMvc mockMvc;
 	
 	@Autowired
     private JdbcTemplate jdbcTemplate;
@@ -40,22 +56,121 @@ public class PhotoFavoriteControllerIntegrationTest {
 		@Test
 		@Order(1)
 		@DisplayName("正常系")
-		void addFavorite_success() throws BadRequestException, RegistFailureException {
-			assertTrue(false);
+		void addFavorite_success() throws Exception {
+			PhotoFavoriteRegistRequest request = new PhotoFavoriteRegistRequest();
+			request.setFavoritePhotoAccountNo(2);
+			request.setFavoritePhotoNo(1);
+			
+			ObjectMapper objectMapper = new ObjectMapper();
+			
+			Account sessionAccount = Account.builder()
+					.accountNo(1)
+					.accountId("aaaaaaaa")
+					.accountName("AAAAAAAA")
+					.password("$2a$10$password1")
+					.authorityKbnCode("administrator")
+					.build();
+			
+			AccountPrincipal accountPrincipal = new AccountPrincipal(sessionAccount, 0);
+			Authentication authentication = new UsernamePasswordAuthenticationToken(accountPrincipal, null);
+			
+			mockMvc.perform(
+					post("/photo/favorite/add")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(objectMapper.writeValueAsString(request))
+					.with(SecurityMockMvcRequestPostProcessors.authentication(authentication))
+					.with(csrf())
+				)
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.httpStatus").value(200))
+				.andExpect(jsonPath("$.isSuccess").value(true))
+				.andExpect(jsonPath("$.message").value("お気に入りに追加しました。"));
+			
+			List<PhotoFavorite> actualData = jdbcTemplate.query(
+					"SELECT * FROM photo.photo_favorite WHERE account_no=1 and favorite_photo_account_no=2 and favorite_photo_no=1", (rs, rowNum) ->
+						PhotoFavorite.builder()
+							.accountNo(rs.getInt("account_no"))
+							.favoritePhotoAccountNo(rs.getInt("favorite_photo_account_no"))
+							.favoritePhotoNo(rs.getInt("favorite_photo_no"))
+							.createdBy(rs.getInt("created_by"))
+							.createdAt(rs.getObject("created_at", OffsetDateTime.class))
+							.build());
+			assertEquals(1, actualData.size());
+			assertEquals(1, actualData.getFirst().getAccountNo());
+			assertEquals(2, actualData.getFirst().getFavoritePhotoAccountNo());
+			assertEquals(1, actualData.getFirst().getFavoritePhotoNo());
+			assertEquals(1, actualData.getFirst().getCreatedBy());
 		}
 		
 		@Test
 		@Order(2)
 		@DisplayName("異常系：BadRequestExceptionをthrowする")
-		void addFavorite_BadRequestException() throws BadRequestException, RegistFailureException {
-			assertTrue(false);
+		void addFavorite_BadRequestException() throws JsonProcessingException, Exception {
+			PhotoFavoriteRegistRequest request = new PhotoFavoriteRegistRequest();
+			request.setFavoritePhotoNo(1);
+			
+			ObjectMapper objectMapper = new ObjectMapper();
+			
+			Account sessionAccount = Account.builder()
+					.accountNo(1)
+					.accountId("aaaaaaaa")
+					.accountName("AAAAAAAA")
+					.password("$2a$10$password1")
+					.authorityKbnCode("administrator")
+					.build();
+			
+			AccountPrincipal accountPrincipal = new AccountPrincipal(sessionAccount, 0);
+			Authentication authentication = new UsernamePasswordAuthenticationToken(accountPrincipal, null);
+			
+			mockMvc.perform(
+					post("/photo/favorite/add")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(objectMapper.writeValueAsString(request))
+					.with(SecurityMockMvcRequestPostProcessors.authentication(authentication))
+					.with(csrf())
+				)
+				.andExpect(status().isBadRequest())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.httpStatus").value(400))
+				.andExpect(jsonPath("$.isSuccess").value(false))
+				.andExpect(jsonPath("$.message").value(ErrorValues.EC0000.getErrorMessage()));
 		}
 		
 		@Test
 		@Order(3)
 		@DisplayName("異常系：RegistFailureExceptionをthrowする")
-		void addFavorite_RegistFailureException() throws BadRequestException, RegistFailureException {
-			assertTrue(false);
+		void addFavorite_RegistFailureException() throws JsonProcessingException, Exception {
+			PhotoFavoriteRegistRequest request = new PhotoFavoriteRegistRequest();
+			request.setFavoritePhotoAccountNo(1);
+			request.setFavoritePhotoNo(1);
+			
+			ObjectMapper objectMapper = new ObjectMapper();
+			
+			Account sessionAccount = Account.builder()
+					.accountNo(1)
+					.accountId("aaaaaaaa")
+					.accountName("AAAAAAAA")
+					.password("$2a$10$password1")
+					.authorityKbnCode("administrator")
+					.build();
+			
+			AccountPrincipal accountPrincipal = new AccountPrincipal(sessionAccount, 0);
+	        Authentication authentication = new UsernamePasswordAuthenticationToken(accountPrincipal, null);
+			
+			mockMvc.perform(
+					post("/photo/favorite/add")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(objectMapper.writeValueAsString(request))
+					.with(SecurityMockMvcRequestPostProcessors.authentication(authentication))
+					.with(csrf())
+				)
+				.andExpect(status().isConflict())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.httpStatus").value(409))
+				.andExpect(jsonPath("$.errorCode").value(ErrorValues.EP0005.getErrorCode()))
+				.andExpect(jsonPath("$.errorMessage").value(ErrorValues.EP0005.getErrorMessage()))
+				.andExpect(jsonPath("$.goBackPageUrl").value("/photo/aaaaaaaa/photo_list"));
 		}
 	}
 	
@@ -67,22 +182,138 @@ public class PhotoFavoriteControllerIntegrationTest {
 		@Test
 		@Order(1)
 		@DisplayName("正常系")
-		void deleteFavorite_success() throws BadRequestException, UpdateFailureException {
-			assertTrue(false);
+		void deleteFavorite_success() throws JsonProcessingException, Exception {
+			PhotoFavoriteDeleteRequest request = new PhotoFavoriteDeleteRequest();
+			request.setFavoritePhotoAccountNo(1);
+			request.setFavoritePhotoNo(1);
+			
+			ObjectMapper objectMapper = new ObjectMapper();
+			
+			Account sessionAccount = Account.builder()
+					.accountNo(1)
+					.accountId("aaaaaaaa")
+					.accountName("AAAAAAAA")
+					.password("$2a$10$password1")
+					.authorityKbnCode("administrator")
+					.build();
+			
+			AccountPrincipal accountPrincipal = new AccountPrincipal(sessionAccount, 0);
+			Authentication authentication = new UsernamePasswordAuthenticationToken(accountPrincipal, null);
+			
+			mockMvc.perform(
+					post("/photo/favorite/delete")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(objectMapper.writeValueAsString(request))
+					.with(SecurityMockMvcRequestPostProcessors.authentication(authentication))
+					.with(csrf())
+				)
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.httpStatus").value(200))
+				.andExpect(jsonPath("$.isSuccess").value(true))
+				.andExpect(jsonPath("$.message").value("お気に入りを解除しました。"));
+			
+			List<PhotoFavorite> actualData = jdbcTemplate.query(
+					"SELECT * FROM photo.photo_favorite WHERE account_no=1 and favorite_photo_account_no=1 and favorite_photo_no=1", (rs, rowNum) ->
+						PhotoFavorite.builder()
+							.accountNo(rs.getInt("account_no"))
+							.favoritePhotoAccountNo(rs.getInt("favorite_photo_account_no"))
+							.favoritePhotoNo(rs.getInt("favorite_photo_no"))
+							.createdBy(rs.getInt("created_by"))
+							.createdAt(rs.getObject("created_at", OffsetDateTime.class))
+							.build());
+			assertEquals(0, actualData.size());
+			
+			List<PhotoFavorite> actualRestData = jdbcTemplate.query(
+					"SELECT * FROM photo.photo_favorite", (rs, rowNum) ->
+						PhotoFavorite.builder()
+							.accountNo(rs.getInt("account_no"))
+							.favoritePhotoAccountNo(rs.getInt("favorite_photo_account_no"))
+							.favoritePhotoNo(rs.getInt("favorite_photo_no"))
+							.createdBy(rs.getInt("created_by"))
+							.createdAt(rs.getObject("created_at", OffsetDateTime.class))
+							.build());
+			
+			assertEquals(3, actualRestData.size());
+			assertEquals(1, actualRestData.get(0).getAccountNo());
+			assertEquals(1, actualRestData.get(0).getFavoritePhotoAccountNo());
+			assertEquals(2, actualRestData.get(0).getFavoritePhotoNo());
+			assertEquals(2, actualRestData.get(1).getAccountNo());
+			assertEquals(1, actualRestData.get(1).getFavoritePhotoAccountNo());
+			assertEquals(2, actualRestData.get(1).getFavoritePhotoNo());
+			assertEquals(2, actualRestData.get(2).getAccountNo());
+			assertEquals(2, actualRestData.get(2).getFavoritePhotoAccountNo());
+			assertEquals(1, actualRestData.get(2).getFavoritePhotoNo());
 		}
 		
 		@Test
 		@Order(2)
 		@DisplayName("異常系：BadRequestExceptionをthrowする")
-		void deleteFavorite_BadRequestException() throws BadRequestException, UpdateFailureException {
-			assertTrue(false);
+		void deleteFavorite_BadRequestException() throws JsonProcessingException, Exception {
+			PhotoFavoriteDeleteRequest request = new PhotoFavoriteDeleteRequest();
+			request.setFavoritePhotoNo(1);
+			
+			ObjectMapper objectMapper = new ObjectMapper();
+			
+			Account sessionAccount = Account.builder()
+					.accountNo(1)
+					.accountId("aaaaaaaa")
+					.accountName("AAAAAAAA")
+					.password("$2a$10$password1")
+					.authorityKbnCode("administrator")
+					.build();
+			
+			AccountPrincipal accountPrincipal = new AccountPrincipal(sessionAccount, 0);
+			Authentication authentication = new UsernamePasswordAuthenticationToken(accountPrincipal, null);
+			
+			mockMvc.perform(
+					post("/photo/favorite/delete")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(objectMapper.writeValueAsString(request))
+					.with(SecurityMockMvcRequestPostProcessors.authentication(authentication))
+					.with(csrf())
+				)
+				.andExpect(status().isBadRequest())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.httpStatus").value(400))
+				.andExpect(jsonPath("$.isSuccess").value(false))
+				.andExpect(jsonPath("$.message").value(ErrorValues.EC0000.getErrorMessage()));
 		}
 		
 		@Test
 		@Order(3)
 		@DisplayName("異常系：UpdateFailureExceptionをthrowする")
-		void deleteFavorite_UpdateFailureException() throws BadRequestException, UpdateFailureException {
-			assertTrue(false);
+		void deleteFavorite_UpdateFailureException() throws JsonProcessingException, Exception {
+			PhotoFavoriteDeleteRequest request = new PhotoFavoriteDeleteRequest();
+			request.setFavoritePhotoAccountNo(99);
+			request.setFavoritePhotoNo(1);
+			
+			ObjectMapper objectMapper = new ObjectMapper();
+			
+			Account sessionAccount = Account.builder()
+					.accountNo(1)
+					.accountId("aaaaaaaa")
+					.accountName("AAAAAAAA")
+					.password("$2a$10$password1")
+					.authorityKbnCode("administrator")
+					.build();
+			
+			AccountPrincipal accountPrincipal = new AccountPrincipal(sessionAccount, 0);
+			Authentication authentication = new UsernamePasswordAuthenticationToken(accountPrincipal, null);
+			
+			mockMvc.perform(
+					post("/photo/favorite/delete")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(objectMapper.writeValueAsString(request))
+					.with(SecurityMockMvcRequestPostProcessors.authentication(authentication))
+					.with(csrf())
+				)
+				.andExpect(status().isConflict())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.httpStatus").value(409))
+				.andExpect(jsonPath("$.errorCode").value(ErrorValues.EP0006.getErrorCode()))
+				.andExpect(jsonPath("$.errorMessage").value(ErrorValues.EP0006.getErrorMessage()))
+				.andExpect(jsonPath("$.goBackPageUrl").value("/photo/aaaaaaaa/photo_list"));
 		}
 	}
 }
