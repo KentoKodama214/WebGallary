97f6913a391e1f166de721715a4163b4e8241075
KentoKodama214 <kodaken214@gmail.com>
2025-06-08 19:26:59 +0900

    開発DBのdockerコンテナ化(2)

--- db/init/init-db.sh
+++ db/init/init-db.sh
@@ -12,9 +12,27 @@ SCHEMA2="photo"
 # psqlコマンドでパスワード入力を省略するために環境変数を利用
 export PGPASSWORD="$PW"
 
+echo "Running initialization scripts..."
+
 # PostgreSQLにスキーマを作成
 psql -U $USER -d $DATABASE -c "CREATE SCHEMA IF NOT EXISTS $SCHEMA1;"
 psql -U $USER -d $DATABASE -c "CREATE SCHEMA IF NOT EXISTS $SCHEMA2;"
 
+for f in /docker-entrypoint-initdb.d/common/*.sql; do
+  if [ -e "$f" ]; then
+    echo "Executing $f"
+    psql -U "$USER" -d "$DATABASE" -f "$f"
+  fi
+done
+
+for f in /docker-entrypoint-initdb.d/photo/*.sql; do
+  if [ -e "$f" ]; then
+    echo "Executing $f"
+    psql -U "$USER" -d "$DATABASE" -f "$f"
+  fi
+done
+
 # 終了後、環境変数を消す場合
 unset PW
+
+echo "Initialization complete."
